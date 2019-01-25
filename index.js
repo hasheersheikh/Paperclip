c202253ea4e04a40ae192b58fa56da12dc9b8617
vigilanti <hasheersheikh@gmail.com>
2019-01-25 23:27:11 +0530

    project added

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+var app = express();
+const path = require('path')
+const PORT = process.env.PORT || 5000
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+  
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('index'))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+// });
+
+// http.listen(PORT, function(){
+//   console.log('listening on ${PORT}');
+// });
